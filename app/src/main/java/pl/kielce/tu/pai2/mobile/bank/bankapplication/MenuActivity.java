2a87d5a14563b7c972cc609b9e2de24639efd7f8
GWojciech <woitek@autograf.pl>
2018-05-10 09:29:14 +0200

    First commit, but we have got sth

---
+++ app/src/main/java/pl/kielce/tu/pai2/mobile/bank/bankapplication/MenuActivity.java
@@ -0,0 +1,13 @@
+package pl.kielce.tu.pai2.mobile.bank.bankapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
---
+++ app/src/main/java/pl/kielce/tu/pai2/mobile/bank/bankapplication/StartActivity.java
@@ -0,0 +1,24 @@
+package pl.kielce.tu.pai2.mobile.bank.bankapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        Button logginButton = (Button) findViewById(R.id.loginButton);
+
+        logginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
